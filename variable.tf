3e3aac27cf8d17565b1f1d22bcb89ebece29e088
shakirhms12 <2020a1r019@mietjammu.in>
2024-12-18 20:12:10 +0530

    add

---
+++ variable.tf
@@ -0,0 +1,24 @@
+variable "instance_type" {
+  description = "Type of the EC2 instance"
+  type        = string
+  default     = "t2.micro"  # Default value (optional)
+}
+
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "aws_access_key_id" {
+  description = "AWS Access Key ID"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Access Key"
+  type        = string
+  sensitive   = true  # Mark as sensitive so it's not displayed in the logs
+}
